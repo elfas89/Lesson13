103b6017e22dad78fd1379086ed4a6465fe30c02
elfas89 <elfas89@gmail.com>
2015-12-13 13:50:16 +0200

    L13

---
+++ Lesson13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson12;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FridgeModes c = FridgeModes.north;
+            Console.WriteLine(c);
+            Fridge z = new Fridge(true, FridgeModes.south);
+        }
+    }
+}
